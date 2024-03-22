dacad5a1caba2a23b4614ba9f902afa8f4157abb
sasank Prajapati <sasank45prajapati@gmail.com>
2024-03-22 17:56:56 +0530

    "implemented auth"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "devFlow",
+  description:
+    "A community - driven plateform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, alogrithms, data structures, and more.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +30,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink : 
+          'primary-text-gradient hover:text-primary-500'
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1>this is piece of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
