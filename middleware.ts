f218c65187f4dd85c8cfc41dfa59161554677148
sasank Prajapati <sasank45prajapati@gmail.com>
2024-05-06 23:44:04 +0530

    "webhook middleware setup"

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 publicRoutes: [
     '/'
-]
+],
+ignoredRoutes: ["/api/webhooks(.*)"],
 });
  
 export const config = {
