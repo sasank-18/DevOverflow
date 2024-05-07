52d09bf835e35b45eb9a77879ae5579270d20798
sasank Prajapati <sasank45prajapati@gmail.com>
2024-05-07 20:21:39 +0530

    "correct the middleware"

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 publicRoutes: [
     '/',
-    '/api/webhook'
+    '/api/webhooks'
 ],
 ignoredRoutes: ["/api/webhooks(.*)"],
 });
